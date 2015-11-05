3e6a0423b2e9edd73e8c98736ecbba0eb8f32159
JVThomas <jthomas0209@gmail.com>
2015-11-05 15:08:22 -0500

    Done.

--- lib/10001st_prime.rb
+++ lib/10001st_prime.rb
@@ -1,1 +1,16 @@
 # Implement your procedural solution here!
+def prime_number_for(nth_element)
+  prime_array = [2,3]
+  counter = 4
+  prime_check = false
+  if nth_element == 1 || nth_element == 2 
+    return prime_array[nth_element - 1]
+  end
+  while prime_array.length < nth_element
+    if [*2..Math.sqrt(counter)].all? {|element| counter%element != 0} == true 
+      prime_array.push(counter)
+    end
+    counter += 1
+  end
+  prime_array[nth_element-1]
+end
--- lib/oo_10001st_prime.rb
+++ lib/oo_10001st_prime.rb
@@ -1,1 +1,21 @@
 # Implement your object-oriented solution here!
+class Prime
+  def initialize(nth_element)
+    @prime_array = [2,3]
+    @index = nth_element
+  end
+
+  def number
+    counter = 4
+    if @index == 1 || @index == 2 
+      return @prime_array[@index - 1]
+    end
+    while @prime_array.length < @index
+      if [*2..Math.sqrt(counter)].all? {|element| counter%element != 0} == true 
+        @prime_array.push(counter)
+      end
+      counter += 1
+    end
+    @prime_array[@index-1]
+  end
+end
